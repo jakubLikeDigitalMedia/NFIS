b0f06f3a3577ceaa9dc122cbc9dbf12e96ac62bf
Dzamil <dzamil.klos@gmail.com>
2014-01-15 16:45:47 +0000

    Signed-off-by: Dzamil <dzamil.klos@gmail.com>

--- scripts/group/group_create.php
+++ scripts/group/group_create.php
@@ -7,18 +7,16 @@
  */
 
 require('../../core_incs.php');
-$_post_group = $_POST['new_group'];
+
+session_start();
+
 //creating a new group
-$group_name = $_post_group;
-$group_table = "`group`";
-$group_model = new Group();
-$group_id = $group_model->createRecord(array("title" => "$group_name"), $group_table);
-
-//adding permissions for the group
-$permissions_table = "`".PE_TABLE."`";
-$permissions_model = new Permissions();
-$group_array = $permissions_model->createGroupArray($group_id, $_POST);// creating a multiple insert
-
-$permissions_model->createRecord($group_array, $permissions_table, array('multiple_insert' => TRUE));
-
-header("Location: " . $_SERVER['HTTP_REFERER'] . "?group_added=true&group_name=$group_name");
+$group = new Group();
+$result = $group->createGroup($_POST);
+$groupName = $_POST['new_group'];
+if(!$result){
+    $_SESSION['user']['inputs']['current_grp'] = $_POST;
+    header("Location: " . $_SERVER['HTTP_REFERER'] . "?group_added=true&group_name=$groupName");
+}else{
+    header("Location: " . $_SERVER['HTTP_REFERER'] . "?group_added=false&group_name=$groupName");
+}
