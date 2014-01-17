ea95d5e96c1ecc6fc35d4ef7c29e60484996b3c0
jacob <jakub.adler@likedigitalmedia.com>
2014-01-17 18:16:22 +0000

    setting and getting inputs and errors from post, getSqlField method

--- models/model_abstract.php
+++ models/model_abstract.php
@@ -11,6 +11,9 @@ abstract class ModelAbstract {
     // DB table info
     protected $primaryKey;
     protected $DbTable;
+    protected static $_DbTable;
+
+    protected $modelName;
 
     protected $dbQueryManager;
     
@@ -25,8 +28,9 @@ abstract class ModelAbstract {
 
     protected function init($primaryKey, $dbTable){
         $this->primaryKey = $primaryKey;
-        $this->DbTable = $dbTable;
+        $this->DbTable = self::$_DbTable = $dbTable;
         $this->dbQueryManager = new DbQueryManager();
+        $this->modelName = $this->getClassName();
 
         // action script initialization
         $this->actionScriptsPathInit();
@@ -38,14 +42,9 @@ abstract class ModelAbstract {
     }
 
     private function actionScriptsPathInit(){
-        $modelName = $this->getClassName();
-        $this->createScriptPath = SCRIPTS."/$modelName/$modelName".'_'.CREATE_SUF.'.php';
-        $this->updateScriptPath = SCRIPTS."/$modelName/$modelName".'_'.UPDATE_SUF.'.php';
-        $this->deleteScriptPath = SCRIPTS."/$modelName/$modelName".'_'.DELETE_SUF.'.php';
-    }
-
-    protected function getClassName(){
-        return strtolower(preg_replace('/(\w)([A-Z])/', '$1_$2', get_class($this)));
+        $this->createScriptPath = SCRIPTS."/{$this->modelName}/{$this->modelName}".'_'.CREATE_SUF.'.php';
+        $this->updateScriptPath = SCRIPTS."/{$this->modelName}/{$this->modelName}".'_'.UPDATE_SUF.'.php';
+        $this->deleteScriptPath = SCRIPTS."/{$this->modelName}/{$this->modelName}".'_'.DELETE_SUF.'.php';
     }
 
     private function formPathInit(){
@@ -54,7 +53,13 @@ abstract class ModelAbstract {
         $this->updateFormPath = FORMS."/$modelName/$modelName".'_'.UPDATE_SUF.'.php';
     }
 
+    protected function getClassName(){
+        return strtolower(preg_replace('/(\w)([A-Z])/', '$1_$2', get_class($this)));
+    }
 
+    public function getSqlQueryField($fieldName){
+        return '`'. $this->DbTable . '`.`'. $fieldName .'`';
+    }
 
     public function getPropertyList($propertyName, $orderType = 'DESC'){
         $query = "SELECT {$this->primaryKey}, $propertyName FROM {$this->DbTable} ORDER BY $propertyName $orderType";
@@ -65,6 +70,11 @@ abstract class ModelAbstract {
     public function createRecord($insertValues, $options = NULL){
         $dbQueryManager = new DbQueryManager();
         return $dbQueryManager->insert($insertValues, "`$this->DbTable`", $options);
+
+    }
+
+    public function getRecords($mysqlQuery, $type = 'array'){
+        return $this->dbQueryManager->selectQuery($mysqlQuery, $this->primaryKey, $type);
 
     }
 
@@ -90,6 +100,43 @@ abstract class ModelAbstract {
         }else{
             return 0;
         }
+    }
+
+    public function recordExist($property){
+        $query =  "SELECT `{$this->primaryKey}`, $property FROM `{$this->DbTable}`";
+        $result = $this->dbQueryManager->selectQuery($query, $this->primaryKey, 'list');
+        return (boolean)sizeof($result) > 0;
+    }
+
+    /*
+     * ========================================================================================
+     * Session handling when working with forms
+     */
+
+    public function saveInputsToSession($inputs){
+        $_SESSION[$this->modelName]['inputs'] = $inputs;
+    }
+
+    public function getInputsFromSession(){
+        (isset($_SESSION[$this->modelName]['inputs']))? $_SESSION[$this->modelName]['inputs']: array();
+    }
+
+    public function saveInputsErrorsToSession($errors){
+        $_SESSION[$this->modelName]['errors'] = $errors;
+    }
+
+    public function getInputsErrorsFromSession(){
+        (isset($_SESSION[$this->modelName]['errors']))? $_SESSION[$this->modelName]['errors']: array();
+    }
+
+    //==========================================================================================
+
+    public function updateMultipleValues($values, $column){
+        $dbc = new DbQueryManager();
+        foreach($values as $value){
+            $column_values[$column . '-' . $value] = $value;
+        }
+        $dbc->update($column_values, '', "`$this->DbTable`", array('multiple_update_one' => TRUE));
     }
 
 
